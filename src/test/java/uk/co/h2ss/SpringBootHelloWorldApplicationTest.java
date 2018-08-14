759be7cbe3e9fed2b9bc8de264ad8d44abe054cd
Saj <saj.issa@gmail.com>
2018-08-14 01:49:47 +0100

    Add Test

---
+++ src/test/java/uk/co/h2ss/SpringBootHelloWorldApplicationTest.java
@@ -0,0 +1,16 @@
+package uk.co.h2ss;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootHelloWorldApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+}
